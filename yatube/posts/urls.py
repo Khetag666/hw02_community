a7b44c4e74a1d898f4cd4182a3cea67ab8231253
Khetag666 <Atlantoc@yandex.ru>
2022-04-16 19:55:29 +0300

    test

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='yatube'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'), #slug
 ]
