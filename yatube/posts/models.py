92e48a0c546666f3ebb3f9978e37e5678636d2a2
Khetag666 <Atlantoc@yandex.ru>
2022-04-16 16:27:26 +0300

    test1

---
+++ yatube/posts/models.py
@@ -0,0 +1,33 @@
+from audioop import reverse
+from tabnanny import verbose
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+    group = models.ForeignKey('Group', blank=True, null=True,
+                              on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True,
+                            verbose_name='posts:group/posts')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'    
